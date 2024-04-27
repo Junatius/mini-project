f53b75f548ae8ef072970dd3b263a53cc2480ac5
Junatius <christophelsoge@gmail.com>
2024-04-27 10:37:04 +0800

    feat: init routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'auth/login', to: 'auth#authenticate'
+
+  resources :conversations, only: [:index, :show] do
+    resources :chats, only: [:index, :create], controller: 'chats', path: 'messages'
+  end
+
+  resources :chats, only: [:create], path: 'messages'
 end
